2e6cb93ee82b2218c0e6628c2686fa5d95913d4d
conankuncs <conankun@gatech.edu>
2016-06-01 11:33:05 +0900

    Initial Commit

---
+++ header.php
@@ -0,0 +1,6 @@
+<?php
+$mysql_ip = "localhost";
+$mysql_id = "root";
+$mysql_pw = "Ivyleague2019@";
+$mysql_db = "buzzmovie";
+?>
---
+++ item.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+	<head>
+		<title>니켈 템셋팅</title>
+		<meta charset='utf8'>
+	</head>
+	<body>
+		<?php
+			phpinfo();	
+		?>
+	</body>
+</html>
